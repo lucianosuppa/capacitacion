fac3b4f553c9f35093e942749a4335c9b383068f
Dario Ocles <dario.ocles@gmail.com>
2019-05-08 10:03:15 -0300

    agregando function

--- Examen-Intermedio-Recu/Dario/Router.php
+++ Examen-Intermedio-Recu/Dario/Router.php
@@ -5,7 +5,19 @@ class Router {
   private $controllers = array();
 
   public function agregarController($path, $controller) {
-    $this->controllers[$path] = $controller;
+    if (!empty($this->controllers[$path])) {
+      $this->controllers[$path] = $controller;
+      return true;
+    }
+    return false;
+  }
+
+  public function deleteController($path) {
+    if (!empty($this->controllers[$path])) {
+      unset($this->controllers[$path]);
+      return true;
+    }
+    return false;
   }
 
   public function dispatch($path) {
